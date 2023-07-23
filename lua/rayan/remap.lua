d5b36ed7cf2385d522f4aa1612f4b58130cdeec1
RAsadi <asadi.rayan@gmail.com>
2023-07-23 12:12:27 -0400

    Adding tree and mappings

--- lua/rayan/remap.lua
+++ lua/rayan/remap.lua
@@ -1,31 +1,38 @@
+local KeyMap = {}
+
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Navigation keys
-vim.keymap.set('n', 'n', 'h', {noremap = true, silent = true})
-vim.keymap.set('n', 'e', 'j', {noremap = true, silent = true})
-vim.keymap.set('n', 'i', 'k', {noremap = true, silent = true})
-vim.keymap.set('n', 'o', 'l', {noremap = true, silent = true})
-vim.keymap.set('v', 'n', 'h', {noremap = true, silent = true})
-vim.keymap.set('v', 'e', 'j', {noremap = true, silent = true})
-vim.keymap.set('v', 'i', 'k', {noremap = true, silent = true})
-vim.keymap.set('v', 'o', 'l', {noremap = true, silent = true})
+vim.keymap.set('', '<leader>pv', ':NvimTreeFocus')
 
-
---  And reverse
-vim.keymap.set('n', 'h', 'n', {noremap = true, silent = true})
-vim.keymap.set('n', 'j', 'e', {noremap = true, silent = true})
-vim.keymap.set('n', 'k', 'i', {noremap = true, silent = true})
-vim.keymap.set('n', 'l', 'o', {noremap = true, silent = true})
-vim.keymap.set('v', 'h', 'n', {noremap = true, silent = true})
-vim.keymap.set('v', 'j', 'e', {noremap = true, silent = true})
-vim.keymap.set('v', 'k', 'i', {noremap = true, silent = true})
-vim.keymap.set('v', 'l', 'o', {noremap = true, silent = true})
+function KeyMap.Colemak()
+  -- Navigation keys
+  vim.keymap.set('n', 'n', 'h', {noremap = true, silent = true})
+  vim.keymap.set('n', 'e', 'j', {noremap = true, silent = true})
+  vim.keymap.set('n', 'i', 'k', {noremap = true, silent = true})
+  vim.keymap.set('n', 'o', 'l', {noremap = true, silent = true})
+  vim.keymap.set('v', 'n', 'h', {noremap = true, silent = true})
+  vim.keymap.set('v', 'e', 'j', {noremap = true, silent = true})
+  vim.keymap.set('v', 'i', 'k', {noremap = true, silent = true})
+  vim.keymap.set('v', 'o', 'l', {noremap = true, silent = true})
 
 
--- Front and back of line
-vim.keymap.set('n', 'N', '0', {noremap = true, silent = true})
-vim.keymap.set('n', 'O', '$', {noremap = true, silent = true})
-vim.keymap.set('v', 'N', '0', {noremap = true, silent = true})
-vim.keymap.set('v', 'O', '$', {noremap = true, silent = true})
+  --  And reverse
+  vim.keymap.set('n', 'h', 'n', {noremap = true, silent = true})
+  vim.keymap.set('n', 'j', 'e', {noremap = true, silent = true})
+  vim.keymap.set('n', 'k', 'i', {noremap = true, silent = true})
+  vim.keymap.set('n', 'l', 'o', {noremap = true, silent = true})
+  vim.keymap.set('v', 'h', 'n', {noremap = true, silent = true})
+  vim.keymap.set('v', 'j', 'e', {noremap = true, silent = true})
+  vim.keymap.set('v', 'k', 'i', {noremap = true, silent = true})
+  vim.keymap.set('v', 'l', 'o', {noremap = true, silent = true})
+
+
+  -- Front and back of line
+  vim.keymap.set('n', 'N', '0', {noremap = true, silent = true})
+  vim.keymap.set('n', 'O', '$', {noremap = true, silent = true})
+  vim.keymap.set('v', 'N', '0', {noremap = true, silent = true})
+  vim.keymap.set('v', 'O', '$', {noremap = true, silent = true})
+end
+
+return KeyMap
 
